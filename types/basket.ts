c8d743f547bf18acf53753876d6e82f86ddfe16c
Matt Driscoll <matt@815dev.com>
2025-07-21 21:04:01 -0500

    Add context to types

--- types/basket.ts
+++ types/basket.ts
@@ -1,8 +1,15 @@
+/**
+ * Represents an online order.
+ * Made up of a basket containing a number of basket entries
+ */
+export type Order = {
+  basket: BasketEntry[];
+};
+
+/**
+ * Represents an item or group of identical items in a order's basket
+ */
 export type BasketEntry = {
   numProducts: number;
   productPrice: number;
-};
-
-export type Order = {
-  basket: BasketEntry[];
 };
