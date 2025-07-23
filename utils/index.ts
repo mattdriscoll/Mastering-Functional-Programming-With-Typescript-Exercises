5d040365abaf948d9a032e69164c7f797e85db65
Matt Driscoll <matt@815dev.com>
2025-07-22 22:18:15 -0500

    Update utils - compose `beginsWith` because why not

--- utils/index.ts
+++ utils/index.ts
@@ -1,4 +1,5 @@
 import { flow } from "fp-ts/lib/function";
+import { pipe } from "../chapters/1-type-classes/exercises";
 
 export const append = (a: string) => (b: string) => b.concat(a);
 export const prepend = (a: string) => (b: string) => a.concat(b);
@@ -7,7 +8,7 @@ export const toArray = <A>(s: A) => Array.of(s);
 export const toString = <A>(a: A) => String(a);
 export const surround = (a: string) => flow(append(a), prepend(a));
 export const beginsWith: (pattern: string) => (a: string) => boolean = pattern => a =>
-  !!a.match(RegExp("^".concat(pattern)));
+  !!a.match(RegExp(pipe(pattern, prepend("^"))));
 
 export const sum = (a: number) => (b: number) => a + b;
 export const multiplyBy = (a: number) => (b: number) => a * b;
